d1b6702946173de39fe7dbef9ada88e3ee7fdffd
Rahul vishwakarma <vishwakarmarahulramesh@gmail.com>
2026-03-19 22:43:49 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,4 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    host: true, 
-    allowedHosts: [
-      'pseudoimpartial-multiply-janeth.ngrok-free.dev'
-    ],
-    proxy: {
-      '/api': {
-        target: 'https://management-backend-a3je.onrender.com', 
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  }
 })
