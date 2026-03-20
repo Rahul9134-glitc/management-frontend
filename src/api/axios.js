7f88be0ac3027b87d70f7760cf03cae051e81916
Rahul vishwakarma <vishwakarmarahulramesh@gmail.com>
2026-03-20 10:01:17 +0530

    change

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,10 +2,5 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL : "https://management-backend-a3je.onrender.com/api/v1",
-    withCredentials : true,
-    headers: {
-        'Content-Type': 'application/json'
-    }
+    withCredentials : true
 });
-
-export default api;
