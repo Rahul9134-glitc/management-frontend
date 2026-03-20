13de7da0c3b88afbb1ff51bb3416990025288760
Rahul vishwakarma <vishwakarmarahulramesh@gmail.com>
2026-03-20 21:45:43 +0530

    change

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://management-backend-a3je.onrender.com',
         changeOrigin: true,
         secure: false
       }
